53915e4c79e5bc4d365f510e9d9edb7560ba953c
Brook072 <shadow820702@gmail.com>
2023-05-04 22:23:32 +0800

    feat:guideline文字設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fontSize: {
+      sm: ['14px', '21px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '30px'],
+      xl: ['24px', '36px'],
+      '2xl': ['28px', '42px'],
+      '3xl': ['32px', '48px'],
+      '4xl': ['40px', '60px'],
+      'heading-base': ['14px', '19.2px'],
+      'heading-lg': ['20px', '24px'],
+      'heading-xl': ['24px', '28.8px'],
+      'heading-2xl': ['28px', '33.6px'],
+      'heading-3xl': ['32px', '38.4px'],
+      'heading-4xl': ['40px', '48px']
+    }
   },
   plugins: []
 }
