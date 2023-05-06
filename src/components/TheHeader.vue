389eeb98602727ce986f7648ddffccf82459d708
Brook072 <shadow820702@gmail.com>
2023-05-06 21:47:15 +0800

    chore:tailwind configuration & feat: part of index layout

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,20 +1,73 @@
 <template>
-  <header class="h-[72px]">
-    <div class="container mx-auto flex h-full items-center justify-between">
-      <div class="me-3">logo</div>
+  <header class="h-auto">
+    <div class="container mx-auto flex h-full items-center justify-between py-2">
+      <RouterLink to="/">
+        <div class="h-11 w-25 bg-logo bg-contain bg-center bg-no-repeat"></div>
+      </RouterLink>
       <nav>
-        <RouterLink to="/course">探索</RouterLink>
+        <div class="flex items-center md:hidden">
+          <span class="material-icons pr-1 text-3xl text-primary-5"> shopping_cart </span>
+          <span class="material-icons text-4xl text-primary-5"> menu </span>
+        </div>
+        <!-- <RouterLink to="/course">探索</RouterLink> -->
       </nav>
-      <template v-if="user">
+      <!-- <div v-if="user">
         <span class="material-icons ms-auto"> search </span>
         <button class="p-3">我的學習</button>
-        <span class="material-icons"> shopping_cart </span>
-      </template>
-      <template v-else>
-        <span class="material-icons ms-auto"> shopping_cart </span>
+        <span class="primary-2 material-icons"> shopping_cart </span>
+      </div>
+      <div v-else>
+        <span class="primary-2 material-icons ms-auto"> shopping_cart </span>
         <button class="p-3" @click="loginModal = true">登入</button>
         <button class="p-3" @click="registerModal = true">註冊</button>
-      </template>
+      </div> -->
+    </div>
+    <!-- side pop-up menu -->
+    <div class="absolute top-0 z-10 hidden h-screen w-full bg-neutral-600 pl-24">
+      <div class="container h-screen w-full bg-neutral-50 pt-3">
+        <div class="flex justify-end">
+          <span class="material-icons text-4xl text-primary-5"> close </span>
+        </div>
+        <div class="mx-auto mb-3.5 h-12 w-27 bg-logo bg-contain bg-center bg-no-repeat"></div>
+        <div class="relative mb-6">
+          <input
+            class="w-full rounded-3xl border border-neutral-200 pr-8 text-heading-3xl"
+            type="text"
+            name=""
+            id=""
+          />
+          <span class="material-icons absolute right-3 top-0.5 text-xl text-primary-5">
+            search
+          </span>
+        </div>
+        <div class="mb-4">
+          <div
+            class="flex items-center justify-center border-b border-b-neutral-100 py-4 text-center text-primary-6"
+          >
+            <span>探索</span>
+            <span class="material-icons">expand_more</span>
+          </div>
+          <RouterLink
+            to=""
+            class="block border-b border-b-neutral-100 py-4 text-center text-primary-6"
+            >線上開課教學</RouterLink
+          >
+          <RouterLink
+            to=""
+            class="block border-b border-b-neutral-100 py-4 text-center text-primary-6"
+            >常見問答</RouterLink
+          >
+          <RouterLink
+            to=""
+            class="block border-b border-b-neutral-100 py-4 text-center text-primary-6"
+            >聯絡我們</RouterLink
+          >
+        </div>
+        <div class="flex justify-between">
+          <button class="btn-primary">登入</button>
+          <button class="btn-secondary">註冊</button>
+        </div>
+      </div>
     </div>
   </header>
   <LoginModal />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,67 @@
 <template>
-  <main>首頁</main>
+  <main>
+    <div class="mb-25 h-94 w-full bg-banner-sm bg-contain bg-center bg-no-repeat"></div>
+    <div class="container">
+      <h2 class="mb-2 text-center text-heading-3xl font-bold text-primary-5">學會做甜點不失敗</h2>
+      <div class="mb-5 flex items-center justify-center">
+        <h2 class="mr-2.5 text-heading-3xl font-bold text-primary-5">只要</h2>
+        <div class="h-7 w-33 bg-intro-site-name bg-contain bg-center bg-no-repeat"></div>
+      </div>
+      <div
+        class="mx-auto mb-5 h-3 w-27 bg-icon-rolling-pin bg-contain bg-center bg-no-repeat"
+      ></div>
+      <p class="mb-4 text-center text-neutral-800">
+        各式各樣的課程，等著充滿熱情的你來探索<br />結合「食 food」智能 AI助教，更能抓準問題的<br />所在，24hr
+        都有他在
+      </p>
+      <p class="mb-6 text-center text-neutral-800">想知道課程到底都教了什麼？查看以下的影片：</p>
+      <div class="mb-13">
+        <div class="relative mb-6 flex h-27 flex-col items-center justify-evenly">
+          <div
+            class="absolute left-1/2 top-0 -ml-13.5 h-27 w-27 rounded-full bg-primary-2 opacity-20"
+          ></div>
+          <p class="text-center text-temporary-1 font-bold text-neutral-800">153</p>
+          <p class="text-center">Chapters</p>
+        </div>
+        <div class="relative mb-6 flex h-27 flex-col items-center justify-evenly">
+          <div
+            class="absolute left-1/2 top-0 -ml-13.5 h-27 w-27 rounded-full bg-primary-2 opacity-20"
+          ></div>
+          <p class="text-center text-temporary-1 font-bold text-neutral-800">63</p>
+          <p class="text-center">Lessons</p>
+        </div>
+        <div class="relative flex h-27 flex-col items-center justify-evenly">
+          <div
+            class="absolute left-1/2 top-0 -ml-13.5 h-27 w-27 rounded-full bg-primary-2 opacity-20"
+          ></div>
+          <p class="text-center text-temporary-1 font-bold text-neutral-800">100+</p>
+          <p class="text-center">hours of videos</p>
+        </div>
+      </div>
+      <div class="mb-6 h-39 w-full bg-video-demo bg-contain bg-center bg-no-repeat"></div>
+    </div>
+    <div class="relative mb-25">
+      <div class="flex gap-6 overflow-x-auto px-3">
+        <div class="w-64">
+          <div class="h-41 w-full rounded-t-xl bg-course-1 bg-cover bg-center bg-no-repeat"></div>
+          <div class="h-22 flex w-full flex-col justify-evenly rounded-b-xl bg-neutral-100 p-2">
+            <p class="mb-2">意式甜點大師班：手工鮮奶油千層蛋糕</p>
+            <p class="text-right text-primary-4">NT$2,680</p>
+          </div>
+        </div>
+        <div class="w-64">
+          <div class="h-41 w-full rounded-t-xl bg-course-1 bg-cover bg-center bg-no-repeat"></div>
+          <div class="h-22 flex w-full flex-col justify-evenly rounded-b-xl bg-neutral-100 p-2">
+            <p class="mb-2">意式甜點大師班：手工鮮奶油千層蛋糕</p>
+            <p class="text-right text-primary-4">NT$2,680</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="bg-feature bg-contain bg-center bg-no-repeat"></div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts"></script>
