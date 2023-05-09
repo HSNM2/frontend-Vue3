8c7960a78cef69e97404236ac8a9fac78e273453
ruby <music24241@gmail.com>
2023-05-09 22:46:46 +0800

    修正 show error ts 錯誤

--- src/composables/useErrorHandler.ts
+++ src/composables/useErrorHandler.ts
@@ -1,11 +1,18 @@
 import Swal from 'sweetalert2'
 import type { AxiosError } from 'axios'
+import axios from 'axios'
 
 export default function useErrorHandler() {
   function showError(err: AxiosError) {
+    if (axios.isAxiosError(err)) {
+      return Swal.fire({
+        icon: 'error',
+        title: err.response?.data?.message || '發生錯誤'
+      })
+    }
     return Swal.fire({
       icon: 'error',
-      title: err.response?.data?.message || '發生錯誤'
+      title: '發生錯誤'
     })
   }
 
