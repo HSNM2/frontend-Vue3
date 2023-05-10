6924124d93e8ffc80979f2940d0c0bbf28e0e663
ruby <music24241@gmail.com>
2023-05-10 14:43:07 +0800

    新增學生dastboard和個人資料頁面

---
+++ src/views/student/DashboardView.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="bg-primary-1 py-8">
+    <div class="container">
+      <div class="grid grid-cols-12 gap-6">
+        <aside class="col-span-3 flex flex-col items-center bg-neutral-50 py-6">
+          <div class="relative">
+            <img src="https://fakeimg.pl/100x100/B7B7B7/" class="mx-auto rounded-full" />
+            <label
+              id="profilePhoto"
+              class="absolute bottom-0 bottom-0 right-0 flex h-7 w-7 cursor-pointer items-center justify-center rounded-full bg-red-50"
+            >
+              <span class="material-icons text-sm">photo_camera</span>
+              <input class="hidden" type="file" name="profile-photo" id="profilePhoto" />
+            </label>
+          </div>
+          <span class="py-6 text-center">會員名字</span>
+          <nav class="w-full border-t py-2">
+            <router-link
+              v-for="link in sidebarMenuLinks"
+              :key="link.to"
+              class="flex items-center justify-center p-4 text-neutral-600 hover:text-primary-4"
+              :class="{ 'text-primary-4': $route.path.startsWith(link.to) }"
+              :to="link.to"
+            >
+              <span class="material-icons me-2"> {{ link.icon }}</span>
+              {{ link.name }}</router-link
+            >
+          </nav>
+        </aside>
+        <div class="col-span-9 bg-primary-1">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const sidebarMenuLinks = ref([
+  { name: '我的學習', icon: 'computer', to: '/student/courses' },
+  { name: '我的收藏', icon: 'bookmark', to: '/student/tags' },
+  { name: '訂單記錄', icon: 'description', to: '/student/orders' },
+  { name: '會員資料', icon: 'person', to: '/student/profile' }
+])
+</script>
--- src/views/student/ProfileView.vue
+++ src/views/student/ProfileView.vue
@@ -1,5 +1,119 @@
 <template>
-  <main>student profile</main>
+  <h1 class="mb-6 bg-neutral-50 px-8 py-4">會員資料</h1>
+  <div class="bg-neutral-50 p-8">
+    <VForm ref="profileForm" class="grid grid-cols-2 gap-6" v-slot="{ meta }" @submit="onSubmit">
+      <div>
+        <label for="name" class="form-label">姓名 <span class="text-rose-500">*</span></label>
+        <VField
+          name="name"
+          rules="required"
+          type="text"
+          v-model.trim="name"
+          label="姓名"
+          v-slot="{ field, errors }"
+        >
+          <input
+            id="name"
+            class="form-control"
+            v-bind="field"
+            :class="{ invalid: !!errors.length }"
+          />
+        </VField>
+        <ErrorMessage class="invalid-feedback" name="name" />
+      </div>
+      <div>
+        <label for="nickName" class="form-label">暱稱 <span class="text-rose-500">*</span></label>
+        <VField
+          name="nickName"
+          type="text"
+          rules="required"
+          v-model.trim="nickName"
+          label="暱稱"
+          v-slot="{ field, errors }"
+        >
+          <input
+            id="nickName"
+            class="form-control"
+            v-bind="field"
+            :class="{ invalid: !!errors.length }"
+          />
+        </VField>
+        <ErrorMessage class="invalid-feedback" name="nickName" />
+      </div>
+      <div>
+        <label for="gender">性別*</label>
+        <VField
+          id="gender"
+          class="form-control"
+          name="gender"
+          rules="required"
+          v-model="gender"
+          label="性別"
+          as="select"
+        >
+          <option value="M">男</option>
+          <option value="F">女</option>
+        </VField>
+        <ErrorMessage class="mb-3 text-red-800" name="gender" />
+      </div>
+      <div class="col-span-2 text-end">
+        <button type="submit" class="btn-primary" :disabled="!meta.valid">提交</button>
+      </div>
+    </VForm>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { nextTick, onMounted, ref } from 'vue'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import { useStatusStore } from '@/stores/status'
+import useErrorHandler from '../../composables/useErrorHandler'
+import { FormContext } from 'vee-validate'
+
+const { updateLoading } = useStatusStore()
+const { showError } = useErrorHandler()
+
+const profileForm = ref<FormContext | null>(null)
+
+const name = ref('')
+const nickName = ref('')
+const gender = ref('M')
+
+function reset() {
+  name.value = ''
+  nickName.value = ''
+  gender.value = 'M'
+
+  nextTick(() => {
+    profileForm.value?.resetForm()
+  })
+}
+
+onMounted(() => {
+  reset()
+})
+
+function onSubmit() {
+  updateLoading(true)
+
+  axios
+    .patch('/api/user/profile', {
+      name: name.value,
+      nickName: nickName.value,
+      gender: gender.value
+    })
+    .then((res) => {
+      Swal.fire({
+        icon: 'success',
+        title: res.data.message
+      })
+    })
+    .catch((err) => {
+      showError(err)
+    })
+    .finally(() => {
+      updateLoading(false)
+    })
+}
+</script>
