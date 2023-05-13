ecdde3655556baba62bc59b259adeb9d60699153
ruby <music24241@gmail.com>
2023-05-13 19:58:22 +0800

    feat:新增header登入後和相關功能

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { storeToRefs } from 'pinia'
 
 import TheHeader from './components/TheHeader.vue'
 import TheFooter from './components/TheFooter.vue'
 
 import { useStatusStore } from './stores/status.js'
-import { storeToRefs } from 'pinia'
+import { useAuthStore } from './stores/auth.js'
 
 const status = useStatusStore()
 const { isLoading } = storeToRefs(status)
+
+const auth = useAuthStore()
+const { getUser } = auth
+
+getUser().catch(() => {})
 </script>
 
 <template>
