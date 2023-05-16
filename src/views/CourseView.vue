9c583b50f075745f95adabc5770e45f0da426fd2
Shila <menai167@gmail.com>
2023-05-16 23:47:52 +0800

    feat: 課程詳細頁面(切版:課程介紹、課程大綱、常見問題)

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -97,6 +97,138 @@
           </div>
         </div>
       </div>
+      <!-- #region 課程介紹 -->
+      <template v-if="tabName === '課程介紹'">
+        <div class="px-3 pb-16">
+          <div class="mb-7">
+            <h4 class="mb-4 text-xl font-bold">關於課程</h4>
+            <div>
+              <div class="mb-2 flex items-center">
+                <span class="material-icons text-lg"> watch_later </span>
+                <p class="mr-4">課程時長</p>
+                <p class="">5時20分</p>
+              </div>
+              <div class="mb-2 flex items-center">
+                <span class="material-icons text-lg"> group </span>
+                <p class="mr-4">課程學員</p>
+                <p class="">200人</p>
+              </div>
+              <div class="flex items-center">
+                <span class="material-icons text-lg"> remove_red_eye </span>
+                <p class="mr-4">觀看期限</p>
+                <p class="">無期限</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h4 class="mb-4 text-xl font-bold">關於簡介</h4>
+            <p class="mb-3 text-lg font-bold">// 你可以學習到</p>
+            <p>
+              本課程旨在讓學生學會製作口感完美的法式馬卡龍，並掌握調配配方、掌握烘焙技巧和控制馬卡龍的口感。
+              以下是本課程的詳細內容：
+              1.馬卡龍的材料和工具：學生可以了解製作馬卡龍所需的材料和工具，例如杏仁粉、砂糖、蛋白、食用色素等。
+              2.馬卡龍的製作流程：學生可以學習如何攪拌、搓團、裝袋、擠花和烘焙馬卡龍。課程中還會詳細介紹每個步驟的重點和技巧。
+              3.馬卡龍的口感掌控：學生可以學習如何控制馬卡龍的口感，例如外皮脆軟、內部柔軟、香甜可口。課程中會講解如何掌握火候和時間，以及如何選擇和配製馬卡龍內餡。
+              4.裝飾和包裝：學生可以學習如何裝飾和包裝馬卡龍，使其更具吸引力和美感。
+              5.實踐練習：課程會提供實踐練習機會，讓學生實際動手製作馬卡龍，並獲得教師的指導和回饋。
+            </p>
+          </div>
+        </div>
+      </template>
+      <!-- #endregion 課程介紹 -->
+      <!-- #region 課程大綱 -->
+      <template v-if="tabName === '課程大綱'">
+        <div class="px-3 pb-16">
+          <h4 class="mb-3 text-xl font-bold">課程大綱</h4>
+          <ul>
+            <li>
+              <div class="mb-4">
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 px-2">
+                  <div class="flex items-center">
+                    <span class="material-icons text-2xl"> list </span>
+                    <p class="text-lg font-bold">第一章 - 瞭解法式馬卡龍</p>
+                  </div>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+                <div class="px-2 py-4">
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>介紹法式馬卡龍的歷史和起源</p>
+                  </div>
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>認識馬卡龍的外觀、口感和成分</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="mb-4">
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 px-2">
+                  <div class="flex items-center">
+                    <span class="material-icons text-2xl"> list </span>
+                    <p class="text-lg font-bold">第一章 - 瞭解法式馬卡龍</p>
+                  </div>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+                <div class="px-2 py-4">
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>介紹法式馬卡龍的歷史和起源</p>
+                  </div>
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>認識馬卡龍的外觀、口感和成分</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="mb-4">
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 px-2">
+                  <div class="flex items-center">
+                    <span class="material-icons text-2xl"> list </span>
+                    <p class="text-lg font-bold">第一章 - 瞭解法式馬卡龍</p>
+                  </div>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+                <div class="px-2 py-4">
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>介紹法式馬卡龍的歷史和起源</p>
+                  </div>
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>認識馬卡龍的外觀、口感和成分</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="mb-4">
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 px-2">
+                  <div class="flex items-center">
+                    <span class="material-icons text-2xl"> list </span>
+                    <p class="text-lg font-bold">第一章 - 瞭解法式馬卡龍</p>
+                  </div>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+                <div class="px-2 py-4">
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>介紹法式馬卡龍的歷史和起源</p>
+                  </div>
+                  <div class="flex items-center py-2">
+                    <span class="material-icons text-lg"> play_arrow </span>
+                    <p>認識馬卡龍的外觀、口感和成分</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </template>
+      <!-- #endregion 課程大綱 -->
       <!-- #region 課前問答 -->
       <template v-if="tabName === '課前問答'">
         <div class="mb-16 grid grid-cols-12 gap-x-6 gap-y-4 px-3">
@@ -226,6 +358,57 @@
       </template>
 
       <!-- #endregion 課前問答 -->
+      <!-- #region 常見問題 -->
+      <template v-if="tabName === '常見問題'">
+        <div class="px-3 pb-16">
+          <div class="mb-16">
+            <h4 class="mb-3 text-xl font-bold">課程相關</h4>
+            <ul class="flex flex-col gap-y-3">
+              <li>
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 p-2">
+                  <p class="text-lg font-bold">
+                    請問這個課程價值 10,000 元，但是網路上評價兩極，請問我該買嗎？
+                  </p>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+                <div class="p-6">
+                  <p>建議可以先看看試閱章節，或是看看相關評價。</p>
+                </div>
+              </li>
+              <li>
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 p-2">
+                  <p class="text-lg font-bold">我剛上小學只有 6 歲，請問我適合來上課嗎？</p>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+              </li>
+              <li>
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 p-2">
+                  <p class="text-lg font-bold">假如我家網路不好，只有 3G，影片還能流暢播放嗎？</p>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4 class="mb-3 text-xl font-bold">退貨機制</h4>
+            <ul class="flex flex-col gap-y-3">
+              <li>
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 p-2">
+                  <p class="text-lg font-bold">如果不小心下單錯誤，請問該如何退款？</p>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+              </li>
+              <li>
+                <div class="flex items-center justify-between gap-x-1 bg-primary-3/50 p-2">
+                  <p class="text-lg font-bold">信用卡多扣 1,200 元，請問該怎麼辦？</p>
+                  <span class="material-icons"> expand_less </span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </template>
+      <!-- #endregion 常見問題 -->
       <!-- #region 課程評價 -->
       <template v-if="tabName === '課程評價'">
         <div class="mb-16 grid grid-cols-12 gap-x-6 gap-y-4 px-3">
@@ -245,7 +428,7 @@
                     <span class="material-icons text-base text-primary-3"> star </span>
                     <span class="material-icons text-base text-primary-3"> star </span>
                   </div>
-                  <div class="h-1 w-7/12 bg-neutral-150"></div>
+                  <div class="bg-neutral-150 h-1 w-7/12"></div>
                 </div>
                 <div class="flex items-center justify-center gap-x-4">
                   <div class="">
@@ -255,7 +438,7 @@
                     <span class="material-icons text-base text-primary-3"> star </span>
                     <span class="material-icons text-base text-primary-3"> star_border </span>
                   </div>
-                  <div class="h-1 w-7/12 bg-neutral-150"></div>
+                  <div class="bg-neutral-150 h-1 w-7/12"></div>
                 </div>
                 <div class="flex items-center justify-center gap-x-4">
                   <div class="">
@@ -265,7 +448,7 @@
                     <span class="material-icons text-base text-primary-3"> star_border </span>
                     <span class="material-icons text-base text-primary-3"> star_border </span>
                   </div>
-                  <div class="h-1 w-7/12 bg-neutral-150"></div>
+                  <div class="bg-neutral-150 h-1 w-7/12"></div>
                 </div>
                 <div class="flex items-center justify-center gap-x-4">
                   <div class="">
@@ -275,7 +458,7 @@
                     <span class="material-icons text-base text-primary-3"> star_border </span>
                     <span class="material-icons text-base text-primary-3"> star_border </span>
                   </div>
-                  <div class="h-1 w-7/12 bg-neutral-150"></div>
+                  <div class="bg-neutral-150 h-1 w-7/12"></div>
                 </div>
                 <div class="flex items-center justify-center gap-x-4">
                   <div class="">
@@ -285,7 +468,7 @@
                     <span class="material-icons text-base text-primary-3"> star_border </span>
                     <span class="material-icons text-base text-primary-3"> star_border </span>
                   </div>
-                  <div class="h-1 w-7/12 bg-neutral-150"></div>
+                  <div class="bg-neutral-150 h-1 w-7/12"></div>
                 </div>
               </div>
               <button type="button" class="btn-primary mx-auto block">
