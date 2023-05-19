6c8ad4a7c1744881498f3290bb805c68de2df83b
Brook072 <shadow820702@gmail.com>
2023-05-20 01:33:57 +0900

    feat: index-layout-finish&course-provider-function-open

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -5,38 +5,54 @@
     class="fixed left-0 right-0 top-0 z-50 h-full max-h-full w-full overflow-y-auto overflow-x-hidden bg-black/30 p-4 md:inset-0"
   >
     <div class="relative m-auto flex h-full max-h-full w-96 items-center">
-      <div ref="modalContent" class="relative w-full bg-white p-6 shadow">
+      <div ref="modalContent" class="relative w-full rounded-md bg-white p-6 shadow">
         <div class="text-end">
           <span @click="loginModal = false" class="material-icons cursor-pointer text-black"
             >close</span
           >
         </div>
         <VForm ref="loginForm" v-slot="{ meta }" @submit="onSubmit">
-          <h2 class="mb-6">登入</h2>
+          <h2 class="mb-6 text-center text-2xl font-bold text-primary-5">歡迎回來！</h2>
           <div class="mb-6">
+            <p class="mb-1">帳號</p>
             <VField
-              class="w-full border"
+              class="w-full rounded-lg border py-2 pl-2 text-lg"
+              id="usernameInput"
               name="email"
               type="email"
               rules="email|required"
               v-model="email"
-              placeholder="信箱"
+              placeholder="請輸入信箱"
+              autocomplete="username"
             />
             <ErrorMessage class="mb-3 text-red-800" name="email" />
           </div>
-          <div class="mb-6">
+          <div class="mb-3">
+            <p class="mb-1">密碼</p>
             <VField
-              class="w-full border"
+              class="w-full rounded-lg border py-2 pl-2 text-lg"
               name="password"
               type="password"
               rules="alpha_num|required|min:8"
-              placeholder="密碼"
+              placeholder="請輸入密碼"
               v-model="password"
               label="密碼"
+              autocomplete="current-password"
             />
             <ErrorMessage class="mb-3 text-red-800" name="password" />
           </div>
-          <button type="submit" class="block w-full p-4 text-center" :disabled="!meta.valid">
+          <div class="mb-8 flex items-center justify-between">
+            <div>
+              <input type="checkbox" id="keep-login" class="mr-2" />
+              <label for="keep-login">維持登入狀態</label>
+            </div>
+            <button class="text-neutral-700 underline">忘記密碼?</button>
+          </div>
+          <button
+            type="submit"
+            class="btn-primary block w-full text-center"
+            :disabled="!meta.valid"
+          >
             登入
           </button>
         </VForm>
@@ -63,6 +79,7 @@ const { loginModal } = storeToRefs(auth)
 
 const modalContent = ref<HTMLInputElement | null>(null)
 const loginForm = ref<FormContext | null>(null)
+// const usernameInput = ref<HTMLElement | null>(null)
 
 watch(loginModal, (newVal) => {
   if (newVal) {
@@ -105,4 +122,8 @@ function handleBackdropClick(event: Event) {
     loginModal.value = false
   }
 }
+
+// watch(usernameInput, () => {
+//   usernameInput.value?.focus()
+// })
 </script>
--- src/components/RegisterModal.vue
+++ src/components/RegisterModal.vue
@@ -5,50 +5,60 @@
     class="fixed left-0 right-0 top-0 z-50 h-full max-h-full w-full overflow-y-auto overflow-x-hidden bg-black/30 p-4 md:inset-0"
   >
     <div class="relative m-auto flex h-full max-h-full w-96 items-center">
-      <div ref="modalContent" class="relative w-full bg-white p-6 shadow">
+      <div ref="modalContent" class="relative w-full rounded-md bg-white p-6 shadow">
         <div class="text-end">
           <span @click="registerModal = false" class="material-icons cursor-pointer text-black"
             >close</span
           >
         </div>
         <VForm ref="registerForm" v-slot="{ meta }" @submit="onSubmit">
-          <h2 class="mb-6">註冊</h2>
+          <h2 class="mb-6 text-center text-2xl font-bold text-primary-5">註冊</h2>
           <div class="mb-6">
+            <p class="mb-1">帳號</p>
             <VField
-              class="w-full border"
+              class="w-full rounded-lg border py-2 pl-2 text-lg"
               name="email"
               type="email"
               rules="email|required"
               v-model="email"
-              placeholder="信箱"
+              placeholder="請輸入信箱"
+              autocomplete="username"
             />
             <ErrorMessage class="mb-3 text-red-800" name="email" />
           </div>
           <div class="mb-6">
+            <p class="mb-1">密碼</p>
             <VField
-              class="w-full border"
+              class="w-full rounded-lg border py-2 pl-2 text-lg"
               name="password"
               type="password"
               rules="alpha_num|required|min:8"
-              placeholder="密碼"
+              placeholder="請輸入密碼"
               v-model="password"
               label="密碼"
+              autocomplete="new-password"
             />
             <ErrorMessage class="mb-3 text-red-800" name="password" />
           </div>
-          <div class="mb-6">
+          <div class="mb-8">
+            <p class="mb-1">密碼確認</p>
             <VField
-              class="w-full border"
+              class="w-full rounded-lg border py-2 pl-2 text-lg"
               name="confirmPassword"
               type="password"
               rules="confirmed:@password"
-              placeholder="確認密碼"
+              placeholder="請再次輸入密碼"
               v-model="confirmPassword"
               label="確認密碼"
+              autocomplete="confirmPassword"
             />
             <ErrorMessage class="mb-3 text-red-800" name="confirmPassword" />
           </div>
-          <button type="submit" class="block w-full p-4 text-center" :disabled="!meta.valid">
+          <button
+            type="submit"
+            class="btn-primary block w-full text-center"
+            :disabled="!meta.valid"
+          >
             註冊
           </button>
         </VForm>
