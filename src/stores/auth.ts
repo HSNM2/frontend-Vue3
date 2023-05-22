cd95d0d19954eb5d0a608e5555ebeb3865f2f1dd
Shila <menai167@gmail.com>
2023-05-22 18:28:55 +0800

    refactor: 封裝axios；新增auth、instructor api檔案

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -15,6 +15,14 @@ interface User {
   avatarImagePath: string
 }
 
+import {
+  LoginRequest,
+  RegisterRequest,
+  IdentityRequest,
+  LogoutRequest,
+  UserRequest
+} from '@/models/auth'
+
 export const useAuthStore = defineStore('auth', () => {
   const user = ref<User | null>(null)
   const identity = ref<User | null>(null)
@@ -25,33 +33,33 @@ export const useAuthStore = defineStore('auth', () => {
   const courseProviderAllowModal = ref(false)
 
   function login(payload: { email: string; password: string }) {
-    return axios.post('/api/user/login', payload).then(() => getUser())
+    return LoginRequest(payload).then(() => getUser())
   }
 
   function register(payload: { email: string; password: string; confirmPassword: string }) {
-    return axios.post('/api/user/register', payload)
+    return RegisterRequest(payload)
   }
 
   function courseProviderAllow(payload: { identityType: number }) {
-    return axios.post('/api/user/identity', payload).then(() => getUser())
+    return IdentityRequest('post', payload).then(() => getUser())
   }
 
   function logout() {
-    return axios.post('/api/user/logout').then((res) => {
+    return LogoutRequest().then((res) => {
       user.value = null
       return res
     })
   }
 
   function getUser() {
-    return axios.get('/api/user/profile').then((res) => {
+    return UserRequest('get', {}).then((res) => {
       user.value = { ...res.data.data }
       return res
     })
   }
 
   function getIdentity() {
-    return axios.get('/api/user/identity').then((res) => {
+    return IdentityRequest('get', {}).then((res) => {
       identity.value = { ...res.data.data }
       return res
     })
