af5a0901871d395d209748c63b82e32b6427d90b
Shila <menai167@gmail.com>
2023-06-16 08:55:08 +0800

    feat: 課程詳細資訊頁面 課程評價api串接

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -395,10 +395,20 @@ const judgeTags = (courseID: number) => {
 //#region 星星
 const getStar = (score: string, index: number) => {
   let rate = Number(score)
-  if (index + 1 <= rate) {
-    return 'star'
+  if (Number.isInteger(rate) === true) {
+    if (index + 1 <= rate) {
+      return 'star'
+    } else {
+      return 'star_border'
+    }
   } else {
-    return 'star_border'
+    if (index + 1 <= rate) {
+      return 'star'
+    } else if (index + 1 - rate === 0.5) {
+      return 'star_half'
+    } else {
+      return 'star_border'
+    }
   }
 }
 //#endregion
