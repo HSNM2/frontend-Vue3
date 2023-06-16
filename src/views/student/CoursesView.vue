9f392a0b0377e3ca018845681592fa0781fa3187
Shila <menai167@gmail.com>
2023-06-16 18:37:41 +0800

    feat: 新增 學生-我的課程 串接api

--- src/views/student/CoursesView.vue
+++ src/views/student/CoursesView.vue
@@ -2,7 +2,7 @@
   <h1 class="mb-6 bg-neutral-50 px-8 py-4 text-lg font-semibold text-neutral-800">我的學習</h1>
   <div class="bg-neutral-50 p-8">
     <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
-      <template v-for="item in courseList?.data" :key="item.id">
+      <template v-for="item in courseList?.courses" :key="item.id">
         <div class="relative">
           <router-link :to="`/learn/${item.id}`">
             <div class="cursor-pointer hover:shadow hover:shadow-md hover:shadow-neutral-150">
@@ -33,11 +33,13 @@
                     </div>
                     <div class="flex flex-wrap">
                       <p class="pe-1 text-sm text-neutral-800">單元進度:</p>
-                      <p class="text-sm text-neutral-800">0完成/10個章節</p>
+                      <p class="text-sm text-neutral-800">
+                        0完成/{{ item.lessons.length + '個章節' }}
+                      </p>
                     </div>
                     <div class="flex flex-wrap">
                       <p class="pe-1 text-sm text-neutral-800">講師:</p>
-                      <p class="text-sm text-neutral-800">{{ item.provider }}</p>
+                      <p class="text-sm text-neutral-800">{{ item.teacherName }}</p>
                     </div>
                     <div class="flex items-center justify-between">
                       <div class="flex items-center">
@@ -66,7 +68,7 @@
           <span class="cursor-pointer px-2" @click="setPage(currentPage - 1)">&lt;</span>
         </div>
         <ul class="flex cursor-pointer items-center">
-          <li v-for="page in courseList?.totalPages" :key="page">
+          <li v-for="page in totalPages" :key="page">
             <div class="border" :class="page === currentPage ? 'bg-primary-4 text-white' : ''">
               <span class="px-2" @click="setPage(page)">{{ page }}</span>
             </div>
@@ -85,14 +87,34 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 import ProgressBar from '@/components/ProgressBar.vue'
 import ReviewModal from '@/components/ReviewModal.vue'
+import { GetUserCoursesRequest } from '@/models/user'
+
+interface Course {
+  title: string
+  image_path: string
+  rating: {
+    avgRating: string
+    countRating: number
+  }
+  teacherName: string
+  lessons: Lesson[]
+}
+
+interface Lesson {
+  id: number
+  title: string
+  isPublish: boolean
+}
+
+// 缺課程id
 
 const minVal = ref(0)
 const maxVal = ref(100)
-const progressVal = ref(15)
+const progressVal = ref(0)
 const progressBarStyle = {
   bg: 'bg-neutral-150',
   progress: 'bg-neutral-500',
@@ -103,121 +125,28 @@ const openReviewModal = () => {
   isShowModal.value = true
 }
 
-const courseList = {
-  data: [
-    {
-      id: 2,
-      title: '馬卡龍風味研究室馬卡龍風味研究室馬卡龍風味研究室馬卡龍風味研究室馬卡龍風味研究室',
-      subTittle: '挑戰法式甜點之最--馬卡龍',
-      image_path: 'http://localhost:3002/static/coverPhoto/coverPhoto-1686669005094-826756121.jpg',
-      price: 3499,
-      originPrice: 3999,
-      link: null,
-      provider: 'amy',
-      tag: '甜點,馬卡龍,法式',
-      buyers: 0,
-      totalTime: 0,
-      courseStatus: '1',
-      type: '法國麵包',
-      category: '麵包',
-      rating: {
-        avgRating: 0,
-        countRating: 0
-      }
-    },
-    {
-      id: 2,
-      title: '馬卡龍風味研究室',
-      subTittle: '挑戰法式甜點之最--馬卡龍',
-      image_path: 'http://localhost:3002/static/coverPhoto/coverPhoto-1686669005094-826756121.jpg',
-      price: 3499,
-      originPrice: 3999,
-      link: null,
-      provider: 'amy',
-      tag: '甜點,馬卡龍,法式',
-      buyers: 0,
-      totalTime: 0,
-      courseStatus: '1',
-      type: '法國麵包',
-      category: '麵包',
-      rating: {
-        avgRating: 0,
-        countRating: 0
-      }
-    },
-    {
-      id: 2,
-      title: '馬卡龍風味研究室',
-      subTittle: '挑戰法式甜點之最--馬卡龍',
-      image_path: 'http://localhost:3002/static/coverPhoto/coverPhoto-1686669005094-826756121.jpg',
-      price: 3499,
-      originPrice: 3999,
-      link: null,
-      provider: 'amy',
-      tag: '甜點,馬卡龍,法式',
-      buyers: 0,
-      totalTime: 0,
-      courseStatus: '1',
-      type: '法國麵包',
-      category: '麵包',
-      rating: {
-        avgRating: 0,
-        countRating: 0
-      }
-    },
-    {
-      id: 2,
-      title: '馬卡龍風味研究室',
-      subTittle: '挑戰法式甜點之最--馬卡龍',
-      image_path: 'http://localhost:3002/static/coverPhoto/coverPhoto-1686669005094-826756121.jpg',
-      price: 3499,
-      originPrice: 3999,
-      link: null,
-      provider: 'amy',
-      tag: '甜點,馬卡龍,法式',
-      buyers: 0,
-      totalTime: 0,
-      courseStatus: '1',
-      type: '法國麵包',
-      category: '麵包',
-      rating: {
-        avgRating: 0,
-        countRating: 0
-      }
-    },
-    {
-      id: 2,
-      title: '馬卡龍風味研究室',
-      subTittle: '挑戰法式甜點之最--馬卡龍',
-      image_path: 'http://localhost:3002/static/coverPhoto/coverPhoto-1686669005094-826756121.jpg',
-      price: 3499,
-      originPrice: 3999,
-      link: null,
-      provider: 'amy',
-      tag: '甜點,馬卡龍,法式',
-      buyers: 0,
-      totalTime: 0,
-      courseStatus: '1',
-      type: '法國麵包',
-      category: '麵包',
-      rating: {
-        avgRating: 0,
-        countRating: 0
-      }
-    }
-  ],
-  page: 1,
-  totalPages: 1
+const courseList = ref<Course | any>({})
+const getData = () => {
+  GetUserCoursesRequest()
+    .then((res) => {
+      courseList.value = res.data.data
+      console.log(courseList.value)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 const currentPage = ref(1)
+const totalPages = ref(1)
 const setPage = (page: number) => {
-  if (page <= 0 || page >= courseList.totalPages) return
+  if (page <= 0 || page >= totalPages.value) return
   currentPage.value = page
 }
 
 //#region 星星
 const getStar = (score: number, index: number) => {
+  console.log(score)
   if (index + 1 <= score) {
     return 'star'
   } else {
@@ -225,4 +154,8 @@ const getStar = (score: number, index: number) => {
   }
 }
 //#endregion
+
+onMounted(() => {
+  getData()
+})
 </script>
