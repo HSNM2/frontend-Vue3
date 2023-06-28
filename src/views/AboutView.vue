19611a750e124cc81a13756b1240aca4535f3734
Shila <menai167@gmail.com>
2023-06-28 22:48:20 +0800

    style: 關於我們 樣式微調

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,7 +5,7 @@
   >
     <div class="container">
       <div class="py-14 text-center md:py-[142px]">
-        <h1 class="mb-6 text-3xl font-bold">關於我們</h1>
+        <h1 class="mb-6 text-heading-3xl font-bold lg:text-heading-4xl">關於我們</h1>
         <p class="text-base">
           開設一個甜點專門知識平台，提供專業的甜點製作教學課程，讓大家更深入認識甜點歷史、文化。
         </p>
@@ -18,13 +18,13 @@
   <div class="container py-[52px] lg:py-[120px]">
     <div class="grid grid-cols-12 gap-x-6">
       <div class="col-span-12 flex flex-col gap-y-5 lg:col-span-6">
-        <h2 class="text-2xl font-bold text-primary-5">
+        <h2 class="text-heading-2xl font-bold text-primary-5 lg:text-heading-3xl">
           讓我們
           <br />
           幫你點燃心中烤爐的火焰
         </h2>
-        <p class="text-lg text-primary-5">「在成為工程師之前，我有個成為烘焙師的夢」</p>
-        <p>
+        <p class="text-lg text-primary-5 lg:text-xl">「在成為工程師之前，我有個成為烘焙師的夢」</p>
+        <p class="text-base">
           糖漬，是一種食物的保存方式，透過在食物中加入大量的糖分，延長食物的保存時間，如同我們想傳達的涵義，在學習的路上有各種可能，而每一次、每個人的學習故事就好比糖漬的過程，在漫長的時間中維持初心，但又風味萬千，希望您能在這裡找到屬於自己獨一無二，猶如「糖漬時光」的個人故事
         </p>
       </div>
@@ -39,9 +39,9 @@
   <div class="bg-primary-1 py-[52px] lg:py-[120px]">
     <div class="container">
       <div class="mb-6 flex flex-col bg-white shadow shadow-md shadow-neutral-150 lg:flex-row">
-        <img src="/image/about-image-2.png" alt="about-image" />
-        <div class="p-5 lg:px-20 lg:py-[42px]">
-          <p class="mb-6 text-xl font-bold text-primary-5">讓製作甜點變得簡單</p>
+        <img class="lg:w-1/2" src="/image/about-image-2.png" alt="about-image" />
+        <div class="p-5 lg:w-1/2 lg:px-20 lg:py-[42px]">
+          <p class="mb-6 text-heading-xl font-bold text-primary-5">讓製作甜點變得簡單</p>
           <p class="text-base text-neutral-800">
             我們相信，甜點製作可以變得更簡單、更高效。讓我們以更簡單的方式幫助您嘗試甜點製作，成為甜點大師。
             我們的課程內容包括了從基礎到高階的所有主題，學員可以在家裡輕鬆製作美味的甜點，並參加與世界各地的甜品愛好者交流的機會，加深烘焙技能，了解不同文化的烘焙傳統。
@@ -51,9 +51,9 @@
       <div
         class="mb-6 flex flex-col bg-white shadow shadow-md shadow-neutral-150 lg:flex-row-reverse"
       >
-        <img src="/image/about-image-3.png" alt="about-image" />
-        <div class="p-5 lg:px-20 lg:py-[42px]">
-          <p class="mb-6 text-xl font-bold text-primary-5">食 food - 智能 AI 助教</p>
+        <img class="lg:w-1/2" src="/image/about-image-3.png" alt="about-image" />
+        <div class="p-5 lg:w-1/2 lg:px-20 lg:py-[42px]">
+          <p class="mb-6 text-heading-xl font-bold text-primary-5">食 food - 智能 AI 助教</p>
           <p class="text-base text-neutral-800">
             我們的智能助手能夠幫助您在眾多食材中找到最佳的搭配，讓您輕鬆創造出令人驚嘆的美食。不論您是有經驗的大廚還是初學者，我們的助手都能提供專業的建議和最佳的食譜。讓我們的智能助手成為您烹飪過程中的得力幫手，讓您的菜餚達到新的高度。
           </p>
@@ -74,7 +74,9 @@
               <span class="material-icons text-4xl text-primary-4"> psychology </span>
               <p class="text-3xl font-bold text-primary-4">AI助教協助</p>
             </div>
-            <p>我們提供食Food助教，就好像烘焙師傅在您身邊，讓您不再大喊「Ｃ父救救我～～</p>
+            <p class="text-base">
+              我們提供食Food助教，就好像烘焙師傅在您身邊，讓您不再大喊「Ｃ父救救我～～
+            </p>
           </div>
         </div>
       </div>
@@ -85,7 +87,9 @@
             <span class="material-icons text-4xl text-primary-4"> cloud_done </span>
             <p class="text-3xl font-bold text-primary-4">課程上架簡易</p>
           </div>
-          <p>經過我們對課程上架流程精心設計，烤肉都還沒發爐，您的課程就已經完成上架了</p>
+          <p class="text-base">
+            經過我們對課程上架流程精心設計，烤肉都還沒發爐，您的課程就已經完成上架了
+          </p>
         </div>
       </div>
       <div class="relative col-span-12 md:col-span-6 lg:col-span-3">
@@ -95,7 +99,9 @@
             <span class="material-icons text-4xl text-primary-4"> style </span>
             <p class="text-3xl font-bold text-primary-4">課程多樣化</p>
           </div>
-          <p>蛋塔、生吐司、泡芙？你想得到的課程我們都有，讓您學到無法停手、烤到無法自</p>
+          <p class="text-base">
+            蛋塔、生吐司、泡芙？你想得到的課程我們都有，讓您學到無法停手、烤到無法自
+          </p>
         </div>
       </div>
       <div class="relative col-span-12 md:col-span-6 lg:col-span-3">
@@ -105,7 +111,9 @@
             <span class="material-icons text-4xl text-primary-4"> devices </span>
             <p class="text-3xl font-bold text-primary-4">介面操作簡單</p>
           </div>
-          <p>我們了解喜歡烘焙的人有很多，設計上的巧思會讓您讚嘆「連我阿嬤都會用！」</p>
+          <p class="text-base">
+            我們了解喜歡烘焙的人有很多，設計上的巧思會讓您讚嘆「連我阿嬤都會用！」
+          </p>
         </div>
       </div>
     </div>
@@ -116,7 +124,7 @@
   <!--#region section5 -->
   <div class="bg-cover md:bg-[url('/image/about-us-bg.png')]">
     <div class="container py-[52px] lg:py-[120px]">
-      <h2 class="mb-3.5 text-center text-3xl font-bold text-primary-5 md:mb-[42px]">
+      <h2 class="mb-3.5 text-center text-heading-3xl font-bold text-primary-5 md:mb-[42px]">
         認識我們的團隊
       </h2>
       <div
@@ -163,12 +171,12 @@
   <!--#endregion section5 -->
 
   <!--#region section6 -->
-  <div class="bg-primary-4/20 py-[52px]">
+  <div class="bg-primary-4/20 py-[52px] lg:py-0">
     <div class="container lg:px-[112px]">
       <div class="flex flex-col gap-y-6 lg:flex-row lg:justify-between">
         <div class="flex flex-col gap-y-4 lg:justify-center">
           <h2 class="text-heading-4xl font-bold text-primary-5">焼きましょ!</h2>
-          <p class="text-3xl">喜愛甜食是人類的天性</p>
+          <p class="text-heading-3xl">喜愛甜食是人類的天性</p>
           <p class="text-base">讓我們一起享受甜食的魅力，一起創造美好的味覺體驗！</p>
           <div class="mt-2">
             <button type="button" class="btn-secondary me-4" @click="enterOtherPage('')">
