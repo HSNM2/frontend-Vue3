5c1e85a44a1fd3329aa3e84f94bc65332e0b4d7c
Shila <menai167@gmail.com>
2023-06-30 23:41:22 +0800

    feat: 固定 header

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,10 @@ const { isLoading } = storeToRefs(status)
 <template>
   <VLoading v-model:active="isLoading" :is-full-page="true" />
   <div class="flex h-screen flex-col">
-    <TheHeader />
-    <div class="flex-1">
+    <div class="fixed top-0 z-50 w-full bg-white shadow shadow-neutral-150">
+      <TheHeader />
+    </div>
+    <div class="mt-[76px] flex-1 md:mt-[64px] lg:mt-[72px]">
       <RouterView />
     </div>
     <TheFooter />
